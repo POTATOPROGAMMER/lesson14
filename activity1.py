1dbf70899208ca7b6897bf2b9fc7942441d301e7
POTATOPROGAMMER <codercode1010@gmail.com>
2025-01-11 12:08:02 +0400

    commit1

---
+++ activity1.py
@@ -0,0 +1,6 @@
+z=int(input("enter the number of rows:"))
+for i in range(z):
+    
+    for j in range (i+1):
+        print("*",end="")
+    print()
---
+++ activity2.py
@@ -0,0 +1,8 @@
+z=int(input("enter the number of rows:"))
+a=1
+for i in range (1,z+1):
+    
+    for k in range(1,i+1):
+        print(a, end=" ")
+        a=a+1
+    print()
